b9a8eb1adb8fcc9bd30b63924e534e9d33e8d47e
Matthew Brooks <mb964033@gmail.com>
2023-11-05 21:16:10 +0000

    allow sqlite.migrate to accept directory name or table

--- lib/santoku/sqlite.lua
+++ lib/santoku/sqlite.lua
@@ -1,7 +1,8 @@
 local gen = require("santoku.gen")
 local compat = require("santoku.compat")
 local err = require("santoku.err")
-local fun = require("santoku.fun")
+local op = require("santoku.op")
+local tup = require("santoku.tuple")
 local fs = require("santoku.fs")
 local sqlite = require("lsqlite3")
 
@@ -264,8 +265,32 @@ M.wrap = function (db)
       end
     end,
 
-    migrate = function (db, fp)
+    migrate = function (db, opts)
       return err.pwrap(function (check)
+
+        local files
+
+        if type(opts) == "string" then
+
+          local fp = opts
+          files = fs.files(fp):map(check):vec():sort():map(function (fp)
+            return tup(fp, function ()
+              return check(fs.readfile(fp))
+            end)
+          end)
+
+        elseif type(opts) == "table" then
+
+          local tbl = opts
+          files = gen.keys(tbl):vec():sort():map(function (fp)
+            return tup(fp, function ()
+              return opts[fp]
+            end)
+          end)
+
+        else
+          return false, "invalid argument type to migrate: " .. type(opts)
+        end
 
         check(db:begin())
 
@@ -279,13 +304,10 @@ M.wrap = function (db)
         local get_migration = check(db:getter("select id from migrations where filename = ?", "id"))
         local add_migration = check(db:inserter("insert into migrations (filename) values (?)"))
 
-        -- TODO: Consider not pulling these all into memory
-        local files = fs.files(fp):map(check):map(fun.nret(1)):vec():sort()
-
-        gen.ivals(files):filter(function (fp)
+        gen.ivals(files):map(op.call):filter(function (fp)
           return not check(get_migration(fp))
-        end):map(function (fp)
-          return fp, check(fs.readfile(fp))
+        end):map(function (fp, read)
+          return fp, read()
         end):each(function (fp, data)
           check(db:exec(data))
           check(add_migration(fp))
