2a0ad96d77012f986adc7704bf24364c39d46c19
broma0 <121064585+broma0@users.noreply.github.com>
2023-12-09 23:21:38 +0000

    update html.lua html.lua

--- test/spec/santoku/html.lua
+++ test/spec/santoku/html.lua
@@ -9,13 +9,13 @@ test("html", function ()
   local tokens = html.parse(text):vec()
 
   assert.same({
-    { start = 1,  position = 19,  text = "this is a test of " },
-    { start = 19, position = 25,  open = "span" },
-    { start = 25, position = 39,  attribute = { name = "class", value = "thing" } },
-    { start = 39, position = 49,  attribute = { name = "id", value = "\"hi\"" } },
-    { start = 49, position = 92,  attribute = { name = "failme", value = "test: \"blah\": it's bound to fail" } },
-    { start = 93, position = 102,  text = "something" },
-    { start = 102, position = 109, close = "span" },
+    { start = 1,   position = 19,   text = "this is a test of " },
+    { start = 19,  position = 25,   open = "span" },
+    { start = 25,  position = 39,   attribute = { name = "class", value = "thing" } },
+    { start = 39,  position = 49,   attribute = { name = "id", value = "\"hi\"" } },
+    { start = 49,  position = 92,   attribute = { name = "failme", value = "test: \"blah\": it's bound to fail" } },
+    { start = 93,  position = 102,  text = "something" },
+    { start = 102, position = 109,  close = "span" },
     n = 7
   }, tokens)
 
