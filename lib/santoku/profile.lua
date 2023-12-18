14db6b2d6eb78bc1cc127189a06f36e64490a728
broma0 <121064585+broma0@users.noreply.github.com>
2023-12-18 19:47:00 +0000

    capi.c profile.lua: add __gc, coroutine.create, coroutine.wrap, hook +2
    lib/santoku/capi.c
      + luaopen_santoku_capi, tk_capi_mt_userdata
    lib/santoku/profile.lua
      + __gc, coroutine.create, coroutine.wrap, hook
    
    also: config.lua

---
+++ lib/santoku/profile.lua
@@ -0,0 +1,50 @@
+local capi = require("santoku.capi")
+local gen = require("santoku.gen")
+local str = require("santoku.string")
+
+local calls, total, this = {}, {}, {}
+
+local function hook (ev)
+  local i = debug.getinfo(2, "Sln")
+  if i.what ~= 'Lua' then return end
+  local fn = table.concat({ i.name or "(unknown)", i.short_src .. ":" .. i.linedefined }, " ")
+  if ev == 'call' then
+    this[fn] = os.clock()
+  elseif this[fn] then
+    local time = os.clock() - this[fn]
+    total[fn] = (total[fn] or 0) + time
+    calls[fn] = (calls[fn] or 0) + 1
+  end
+end
+
+debug.sethook(hook, "cr")
+
+local co_create = coroutine.create
+
+coroutine.create = function (...) -- luacheck: ignore
+  local co = co_create(...)
+  debug.sethook(co, hook, "cr")
+  return co
+end
+
+coroutine.wrap = function (...) -- luacheck: ignore
+  local co = co_create(...)
+  debug.sethook(co, hook, "cr")
+  return function (...)
+    return coroutine.resume(co, ...)
+  end
+end
+
+_G[capi.userdata({
+
+  __gc = function ()
+    gen.pairs(total):map(function (fn, time)
+      return { fn = fn, time = time, calls = calls[fn] }
+    end):vec():sort(function (a, b)
+      return a.time > b.time
+    end):each(function (d)
+      str.printf("%.4f\t%d\t%s\n", d.time, d.calls, d.fn)
+    end)
+  end
+
+})] = true
