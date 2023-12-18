3ae95535494bb12b8584b6bd6d43910546581e5e
Matthew Brooks <mb964033@gmail.com>
2023-12-18 19:47:00 +0000

    add santoku.profile and santoku.capi

---
+++ lib/santoku/capi.c
@@ -0,0 +1,24 @@
+#include "lua.h"
+#include "lauxlib.h"
+
+int tk_capi_mt_userdata (lua_State *L)
+{
+  luaL_checktype(L, -1, LUA_TTABLE);
+  lua_newuserdata(L, 0);
+  lua_insert(L, -2);
+  lua_setmetatable(L, -2);
+  return 1;
+}
+
+luaL_Reg tk_capi_mt_fns[] =
+{
+  { "userdata", tk_capi_mt_userdata },
+  { NULL, NULL }
+};
+
+int luaopen_santoku_capi (lua_State *L)
+{
+  lua_newtable(L); // mt
+  luaL_register(L, NULL, tk_capi_mt_fns); // mt
+  return 1;
+}
