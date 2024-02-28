45893a3bac81ffed5599f34e05dbb8fa1547245d
broma0 <121064585+broma0@users.noreply.github.com>
2024-02-28 04:21:48 +0000

    base.c: add tk_string_base_equals, tk_string_base_number
    lib/santoku/string/base.c
      + tk_string_base_equals
      ~ tk_string_base_number
    
    also: lib/santoku/string.lua, make.lua, test/spec/santoku/string.lua

--- lib/santoku/string/base.c
+++ lib/santoku/string/base.c
@@ -2,8 +2,10 @@
 #include "lauxlib.h"
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-int tk_string_base_number (lua_State *L)
+static int tk_string_base_number (lua_State *L)
 {
   lua_settop(L, 3);
   size_t len;
@@ -21,9 +23,35 @@ int tk_string_base_number (lua_State *L)
   }
 }
 
-luaL_Reg tk_string_mt_fns[] =
+static int tk_string_base_equals (lua_State *L)
+{
+  lua_settop(L, 4);
+  size_t litlen;
+  const char *lit = luaL_checklstring(L, 1, &litlen);
+  size_t chunklen;
+  const char *chunk = luaL_checklstring(L, 2, &chunklen);
+  lua_Integer s = luaL_checkinteger(L, 3);
+  if (s < 1) {
+    lua_pushboolean(L, false);
+    return 1;
+  }
+  if (s > chunklen) {
+    lua_pushboolean(L, false);
+    return 1;
+  }
+  lua_Integer e = luaL_checkinteger(L, 4);
+  if (e < 1) {
+    lua_pushboolean(L, false);
+    return 1;
+  }
+  lua_pushboolean(L, strncmp(lit, chunk + s - 1, e - s + 1) == 0);
+  return 1;
+}
+
+static luaL_Reg tk_string_mt_fns[] =
 {
   { "number", tk_string_base_number },
+  { "equals", tk_string_base_equals },
   { NULL, NULL }
 };
 
