bb7bc95abe9ddfca2b8305111f7224d58eea9e5c
broma0 <121064585+broma0@users.noreply.github.com>
2024-03-04 15:51:35 +0000

    iter.lua: add max, mean, min, set
    lib/santoku/iter.lua
      + max, mean, min, set
    
    also: make.lua, test/spec/santoku/iter.lua

--- lib/santoku/iter.lua
+++ lib/santoku/iter.lua
@@ -305,9 +305,51 @@ local function sum (it)
 end
 
 local function count (it)
+  assert(hascall(it))
   return reduce(function (a)
     return a + 1
   end, 0, it)
+end
+
+local function min (it)
+  assert(hascall(it))
+  return reduce(function (a, b)
+    if not a or b < a then
+      return b
+    else
+      return a
+    end
+  end, nil, it)
+end
+
+local function set (it, t)
+  t = t or {}
+  assert(hascall(it))
+  assert(hasindex(t))
+  return reduce(function (a, n)
+    a[n] = true
+    return a
+  end, t, it)
+end
+
+local function max (it)
+  assert(hascall(it))
+  return reduce(function (a, b)
+    if not a or b > a then
+      return b
+    else
+      return a
+    end
+  end, nil, it)
+end
+
+local function mean (it)
+  assert(hascall(it))
+  local c = 0
+  return reduce(function (a, b)
+    c = c + 1
+    return a + b
+  end, 0, it) / c
 end
 
 local function drop (n, it)
@@ -423,6 +465,10 @@ return {
   tabulate = tabulate,
   sum = sum,
   count = count,
+  min = min,
+  max = max,
+  mean = mean,
+  set = set,
 
   interleave = interleave,
   deinterleave = deinterleave,
@@ -443,36 +489,6 @@ return {
   take = take,
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
