3df713b8fe74c5e0c17a77d2d460b5307169e6fd
Matthew Brooks <mb964033@gmail.com>
2024-04-21 01:51:15 +0000

    begin accumulating useful bits of c for lua

---
+++ res/utils.c
@@ -0,0 +1,77 @@
+static inline void tk_lua_callmod (lua_State *L, int nargs, int nret, const char *smod, const char *sfn)
+{
+  lua_getglobal(L, "require"); // arg req
+  lua_pushstring(L, smod); // arg req smod
+  lua_call(L, 1, 1); // arg mod
+  lua_pushstring(L, sfn); // args mod sfn
+  lua_gettable(L, -2); // args mod fn
+  lua_remove(L, -2); // args fn
+  lua_insert(L, - nargs - 1); // fn args
+  lua_call(L, nargs, nret); // results
+}
+
+static inline FILE *tk_lua_fopen (lua_State *L, const char *fp, const char *flag)
+{
+  FILE *fh = fopen(fp, flag);
+  if (fh) return fh;
+  int e = errno;
+  lua_settop(L, 0);
+  lua_pushstring(L, "Error opening file");
+  lua_pushstring(L, fp);
+  lua_pushstring(L, strerror(e));
+  lua_pushinteger(L, e);
+  tk_lua_callmod(L, 4, 0, "santoku.error", "error");
+  return NULL;
+}
+
+static inline void tk_lua_fclose (lua_State *L, FILE *fh)
+{
+  if (!fclose(fh)) return;
+  int e = errno;
+  lua_settop(L, 0);
+  lua_pushstring(L, "Error closing file");
+  lua_pushstring(L, strerror(e));
+  lua_pushinteger(L, e);
+  tk_lua_callmod(L, 3, 0, "santoku.error", "error");
+  return NULL;
+}
+
+static inline void tk_lua_fwrite (lua_State *L, char *data, size_t size, size_t memb, FILE *fh)
+{
+  size_t read = fwrite(fh, data, size, memb);
+  if (!ferror(fh)) return;
+  int e = errno;
+  lua_settop(L, 0);
+  lua_pushstring(L, "Error writing to file");
+  lua_pushstring(L, strerror(e));
+  lua_pushinteger(L, e);
+  tk_lua_callmod(L, 3, 0, "santoku.error", "error");
+}
+
+static inline void tk_lua_fread (lua_State *L, void *data, size_t size, size_t memb, FILE *fh)
+{
+  fread(data, size, memb, fh);
+  if (!ferror(fh)) return;
+  int e = errno;
+  lua_settop(L, 0);
+  lua_pushstring(L, "Error reading from file");
+  lua_pushstring(L, strerror(e));
+  lua_pushinteger(L, e);
+  tk_lua_callmod(L, 3, 0, "santoku.error", "error");
+}
+
+static inline int tk_lua_ref (lua_State *L, int i)
+{
+  lua_pushvalue(L, i);
+  return luaL_ref(L, LUA_REGISTRYINDEX, -1);
+}
+
+static inline void tk_lua_unref (lua_State *L, int r)
+{
+  luaL_unref(L, LUA_REGISTRYINDEX, r);
+}
+
+static inline void tk_lua_deref (lua_State *L, int r)
+{
+  lua_rawgeti(L, LUA_REGISTRYINDEX, r);
+}
