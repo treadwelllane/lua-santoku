5585d181aed27a3d9c5a25134eab4af9bc2c2abc
Matthew Brooks <mb964033@gmail.com>
2024-10-20 19:15:48 +0000

    add encoder/decoders for hex, base64, base64_url, and url

--- test/spec/santoku/string.lua
+++ test/spec/santoku/string.lua
@@ -8,6 +8,9 @@ local tbl = require("santoku.table")
 local teq = tbl.equals
 local tmap = tbl.map
 
+local vdt = require("santoku.validate")
+local eq = vdt.isequal
+
 local str = require("santoku.string")
 local ssplit = str.splits
 local smatch = str.matches
@@ -140,4 +143,55 @@ test("equals", function ()
   assert(teq({ false }, { str.equals("one", "one two three", -10, 3) }));
   assert(teq({ false }, { str.equals("one", "one two three", 0, 3) }));
   assert(teq({ false }, { str.equals("one", "one two three", 3, 1) }));
+end)
+
+test("to/from_hex", function ()
+  local s = "this is an easy test"
+  assert(eq(s, str.from_hex(str.to_hex(s))))
+  assert(eq("", str.from_hex(str.to_hex("")))) -- Empty string
+  assert(eq(" ", str.from_hex(str.to_hex(" ")))) -- Single space
+  assert(eq("!", str.from_hex(str.to_hex("!")))) -- Single character
+  assert(eq("1234567890", str.from_hex(str.to_hex("1234567890")))) -- Numeric string
+  assert(eq("\xFF\xFE\xFD", str.from_hex(str.to_hex("\xFF\xFE\xFD")))) -- Non-printable bytes
+  assert(eq("\x00\x01\x02\x03", str.from_hex(str.to_hex("\x00\x01\x02\x03")))) -- Leading zeros
+end)
+
+test("to/from_base64", function ()
+  local s = "this is an easy test"
+  assert(eq(s, str.from_base64(str.to_base64(s))))
+  assert(eq("", str.from_base64(str.to_base64("")))) -- Empty string
+  assert(eq("A", str.from_base64(str.to_base64("A")))) -- Single character
+  assert(eq("AB", str.from_base64(str.to_base64("AB")))) -- Two characters
+  assert(eq("ABC", str.from_base64(str.to_base64("ABC")))) -- Three characters
+  assert(eq("ABCD", str.from_base64(str.to_base64("ABCD")))) -- Four characters
+  assert(eq("\xFF\xFE\xFD", str.from_base64(str.to_base64("\xFF\xFE\xFD")))) -- Non-printable bytes
+  assert(eq("Man", str.from_base64("TWFu"))) -- Valid base64 for "Man"
+  assert(eq("Man", str.from_base64("TWFu=="))) -- Base64 with padding
+end)
+
+test("to/from_base64_url", function ()
+  local s = "this is an easy test"
+  assert(eq(s, str.from_base64_url(str.to_base64_url(s))))
+  assert(eq("", str.from_base64_url(str.to_base64_url("")))) -- Empty string
+  assert(eq("A", str.from_base64_url(str.to_base64_url("A")))) -- Single character
+  assert(eq("AB", str.from_base64_url(str.to_base64_url("AB")))) -- Two characters
+  assert(eq("ABC", str.from_base64_url(str.to_base64_url("ABC")))) -- Three characters
+  assert(eq("ABCD", str.from_base64_url(str.to_base64_url("ABCD")))) -- Four characters
+  assert(eq("\xFF\xFE\xFD", str.from_base64_url(str.to_base64_url("\xFF\xFE\xFD")))) -- Non-printable bytes
+  assert(eq("Man", str.from_base64_url("TWFu"))) -- Valid base64 URL for "Man"
+  assert(eq("Man", str.from_base64_url("TWFu=="))) -- Base64 URL with padding
+  assert(eq(
+    "AD55961CE994BB017566DFCF04DF81A489BF6B48BC9A9C1BEAF7308A44DC31A550C98AA79348A91ABB8CC906C48295321791C792DBF67FE6795593963C01B29BD179E6EBD83B41BF20F30DCDCE8A291C24B5C81A7B730E2AB1BEFA1B55EC7B469E6AA624E956B36E810B7BD8682E37891BB53202BFA46D6B9790EA113689CD87B1F46ABBF26B6151AC76816D5CCB6EDA83781374B8CE37BC478C767E2CCC61F7DD5D78913F0068DFB3325C3BA238A2599EB59A854EC56DFB0D55E70824980BA499A91336B47892D23A98DB10023AF859167CA531B94C32EA8C94FCA46D615246286747433FAD1B9078A81B14F736652BD48AA25421BCE6F63ADF047B1CDAD05F", -- luacheck: ignore
+    str.to_hex(str.from_base64_url("rVWWHOmUuwF1Zt_PBN-BpIm_a0i8mpwb6vcwikTcMaVQyYqnk0ipGruMyQbEgpUyF5HHktv2f-Z5VZOWPAGym9F55uvYO0G_IPMNzc6KKRwktcgae3MOKrG--htV7HtGnmqmJOlWs26BC3vYaC43iRu1MgK_pG1rl5DqETaJzYex9Gq78mthUax2gW1cy27ag3gTdLjON7xHjHZ-LMxh991deJE_AGjfszJcO6I4olmetZqFTsVt-w1V5wgkmAukmakTNrR4ktI6mNsQAjr4WRZ8pTG5TDLqjJT8pG1hUkYoZ0dDP60bkHioGxT3NmUr1IqiVCG85vY63wR7HNrQXw")) -- luacheck: ignore
+  ))
+end)
+
+test("to/from_url", function ()
+  local s = "this is an easy test"
+  assert(eq(s, str.from_url(str.to_url(s))))
+  assert(eq("", str.from_url(str.to_url("")))) -- Empty string
+  assert(eq(" ", str.from_url(str.to_url(" ")))) -- Single space
+  assert(eq("!@#$%^&*()", str.from_url(str.to_url("!@#$%^&*()")))) -- Special characters
+  assert(eq("A simple test with   spaces",
+    str.from_url(str.to_url("A simple test with   spaces")))) -- URL encoding spaces
 end)
