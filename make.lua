38ae4507f29c51ea037d438a0b3fad2a8909c9ee
broma0 <121064585+broma0@users.noreply.github.com>
2025-01-15 18:56:54 +0000

    async.lua: add M.events, M.id, emit, off, on, process, run_process
    lib/santoku/async.lua
      + M.events, M.id, emit, off, on, process, run_process
    
    also: make.lua, test/bench/bench.lua, test/spec/santoku/async.lua

--- make.lua
+++ make.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku",
-  version = "0.0.216-1",
+  version = "0.0.217-1",
   variable_prefix = "TK",
   license = "MIT",
   public = true,
--- test/bench/bench.lua
+++ test/bench/bench.lua
@@ -1,42 +1,35 @@
-local vec = require("santoku.vector")
-local plist = require("pl.List")
 
-collectgarbage("stop")
 
-local run = function(label, fn)
+local bench = require("santoku.bench")
+local rand = require("santoku.random")
 
-  collectgarbage("collect")
-  local sm = collectgarbage("count")
-  local st = os.clock()
-
-  fn()
-
-  local et = os.clock()
-  local em = collectgarbage("count")
-
-  print(label .. " time", et - st)
-  print(label .. " mem", em - sm)
-
+local data = {}
+for i = 1, 100000 do
+  data[i] = rand.num()
 end
 
-run("plist", function ()
-  local l = plist.new()
-  for i = 1, 1000000 do
-
-    l:push(i)
+bench("numeric for", function ()
+  local total = 0
+  for i = 1, #data do
+    total = total + data[i]
   end
+  return total
 end)
 
-run("vector", function ()
-  local l = vec()
-  for i = 1, 1000000 do
-    l:append(i)
+bench("it.ivals", function ()
+  local total = 0
+  for v in it.ivals(data) do
+    total = total + v
   end
+  return total
 end)
 
-run("table", function ()
-  local t = {}
-  for i = 1, 1000000 do
-    t[i] = i
-  end
+bench("asy.ivals", function ()
+  return asy.ivals(function (k, v, t)
+    if v == nil then
+      return k(nil, t)
+    else
+      return k(v, t + v)
+    end
+  end, data, 0)
 end)
