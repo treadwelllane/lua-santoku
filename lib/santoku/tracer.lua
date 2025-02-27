04a961c0919243e9b967c4a704d6ca0d7ddb2bc3
broma0 <121064585+broma0@users.noreply.github.com>
2025-02-27 15:52:47 +0000

    update trace.lua tracer.lua

--- lib/santoku/tracer.lua
+++ lib/santoku/tracer.lua
@@ -1,3 +1,8 @@
+
+
+
+
+
 local str = require("santoku.string")
 local it = require("santoku.iter")
 local fs = require("santoku.fs")
@@ -29,4 +34,7 @@ end
 
 return function ()
   debug.sethook(trace, "rl")
+  return function ()
+    debug.sethook()
+  end
 end
