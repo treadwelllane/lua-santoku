d77c1f80f6d7b4a7f20a28a0fdf7c4ffd9954d3c
broma0 <121064585+broma0@users.noreply.github.com>
2025-02-27 15:48:44 +0000

    update make.lua trace.lua tracer.lua

---
+++ lib/santoku/trace.lua
@@ -0,0 +1,1 @@
+require("santoku.profiler")()
---
+++ lib/santoku/tracer.lua
@@ -0,0 +1,32 @@
+local str = require("santoku.string")
+local it = require("santoku.iter")
+local fs = require("santoku.fs")
+
+local line_cache = {}
+local name_cache = {}
+
+local trace = function (_, line)
+  local i = debug.getinfo(2, "Sn")
+  local source = i.source and str.match(i.source, "^@(.*)")
+  if source then
+    local ls = line_cache[source]
+    if not ls then
+      ls = it.collect(fs.lines(source))
+      line_cache[source] = ls
+    end
+    local bn = name_cache[source]
+    if not bn then
+      bn = fs.basename(source)
+      name_cache[bn] = bn
+    end
+    if line then
+      str.printf("%20s  %4d  %s\n", bn, line, ls[line])
+    elseif i.name then
+      str.printf("%20s  %4s  %s\n", bn, "", i.name)
+    end
+  end
+end
+
+return function ()
+  debug.sethook(trace, "rl")
+end
