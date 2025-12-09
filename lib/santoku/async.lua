bb066ca7ee14e7109c628bc061b9cd1247c878cc
broma0 <121064585+broma0@users.noreply.github.com>
2025-12-09 09:47:26 -0500

    error.lua iter.lua num.lua varg.lua: update _reduce, interleave, reduce +22
    lib/santoku/error.lua
      + wrap_result
      - _wrapnil, _wrapok
    lib/santoku/iter.lua
      + step
      - _async, _key, _reduce, _spread, _val, helper, helper0, helper1, wrap
      ~ find, interleave, pairs, range, reduce
    lib/santoku/num.lua
      ~ atan
    lib/santoku/varg.lua
      - _extend, _interleave, _reduce, _reverse
      ~ extend, get, interleave, reduce, set
    
    also: lib/santoku/array.lua, lib/santoku/async.lua, lib/santoku/functional.lua, lib/santoku/table.lua, lib/santoku/validate.lua, test/spec/santoku/array.lua, test/spec/santoku/table.lua

--- lib/santoku/async.lua
+++ lib/santoku/async.lua
@@ -1,7 +1,5 @@
-local varg = require("santoku.varg")
 local arr = require("santoku.array")
 local fun = require("santoku.functional")
-local tup = varg.tup
 
 local M = {}
 
@@ -15,29 +13,24 @@ M._pipe = function (n, fns, ok, ...)
   end
 end
 
-
-
-
-
 M.pipe = function (...)
   local fns = { ... }
   return M._pipe(1, fns, true)
 end
 
 M._each = function (g, it, done)
-  return tup(function (...)
-    if ... == nil then
-      return done(true)
-    else
-      return it(function (ok, ...)
-        if not ok then
-          return done(ok, ...)
-        else
-          return M._each(g, it, done)
-        end
-      end, ...)
-    end
-  end, g())
+  local v = g()
+  if v == nil then
+    return done(true)
+  else
+    return it(function (ok, ...)
+      if not ok then
+        return done(ok, ...)
+      else
+        return M._each(g, it, done)
+      end
+    end, v)
+  end
 end
 
 M.each = function (g, it, done)
@@ -50,8 +43,6 @@ M._iter = function (y, it, done)
       if not ok then
         return done(ok, ...)
       else
-
-
         return M._iter(y, it, done)
       end
     end, ...)
