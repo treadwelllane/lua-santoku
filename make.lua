2500e8c0aaa4f062dc320956f76ddcc719c2deb1
broma0 <121064585+broma0@users.noreply.github.com>
2025-12-26 21:02:36 -0500

    error.lua: remove copcall, copcall_handle, coxpcall, coxpcall_handle
    lib/santoku/error.lua
      - copcall, copcall_handle, coxpcall, coxpcall_handle
    
    also: make.lua

--- make.lua
+++ make.lua
@@ -1,6 +1,6 @@
 local env = {
   name = "santoku",
-  version = "0.0.317-1",
+  version = "0.0.318-1",
   variable_prefix = "TK",
   license = "MIT",
   public = true,
@@ -20,7 +20,4 @@ env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
 env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
-return {
-
-  env = env,
-}
+return { env = env }
